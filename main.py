75f54f6709004517803be91a8b74fa28e2220b92
Domen Oslaj <doslaj@gmail.com>
2017-03-16 09:56:14 +0100

    Program asks for dish name and price

---
+++ main.py
@@ -0,0 +1,7 @@
+print "Hi and wellcome to the our menu program"
+
+menu = {}
+
+while True:
+    add_dish = raw_input( "Please enter the name of the dish" )
+    add_price = raw_input( "Please enter the price for " + add_dish )
